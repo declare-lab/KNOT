f047b2ee0e3079f5005062373f8060bd506f4f7a
Rishabh Bhardwaj <32847115+Bhardwaj-Rishabh@users.noreply.github.com>
2021-10-19 16:57:25 +0800

    Add files via upload

---
+++ Sentiment/src/bash.sh
@@ -0,0 +1,30 @@
+#Sentiment task
+
+#Note: make sure all models should appear in order:
+#  loc Cell phones -> loc Clothing -> loc Toys (-> glob Food)
+
+#pretrain local models
+  #entropy-based
+CUDA_VISIBLE_DEVICES=1 python run_local_classifier.py  -loss_fn='entropy' -dataset='../train_test_split/loc_Cell_Phones_and_Accessories.pkl' -batch_size=64 -epochs=10 -lr=0.0005 -max_text_len=200 -save_model_path='../saved_models' -metric='f1_macro' -result_path='../results.txt' -model_type='local'
+CUDA_VISIBLE_DEVICES=2 python run_local_classifier.py  -loss_fn='entropy' -dataset='../train_test_split/loc_Clothing_Shoes_and_Jewelry.pkl' -batch_size=64 -epochs=10 -lr=0.0005 -max_text_len=200 -save_model_path='../saved_models' -metric='f1_macro' -result_path='../results.txt' -model_type='local'
+CUDA_VISIBLE_DEVICES=3 python run_local_classifier.py  -loss_fn='entropy' -dataset='../train_test_split/loc_Toys_and_Games.pkl' -batch_size=64 -epochs=10 -lr=0.0005 -max_text_len=200 -save_model_path='../saved_models' -metric='f1_macro' -result_path='../results.txt' -model_type='local'
+
+
+#pretrain global model
+  #entropy-based
+CUDA_VISIBLE_DEVICES=2 python run_local_classifier.py -loss_fn='entropy' -dataset='../train_test_split/globP_set_reviews_Food.pkl' -batch_size=2048 -epochs=5 -lr=0.0005 -max_text_len=200 -save_model_path='../saved_models' -metric='f1_macro' -result_path='../results.txt' -model_type='global'
+
+
+#create noisy labels from local models on transfer set
+CUDA_VISIBLE_DEVICES=2 python run_noisy_labels.py -tranfer_set='../train_test_split/globF_Grocery_and_Gourmet_Food.pkl' -batch_size=1024 -save_model_path='../noisy_labels' -lm='../saved_models/best_model_entropy_loc_Cell_Phones_and_Accessories.pt' -lm='../saved_models/best_model_entropy_loc_Clothing_Shoes_and_Jewelry.pt' -lm='../saved_models/best_model_entropy_loc_Toys_and_Games.pt' -gm='../saved_models/best_model_entropy_globP_set_reviews_Food.pt'
+
+
+#distribution bias
+CUDA_VISIBLE_DEVICES=3 python calc_dist_bias.py -batch_size=128 -lm='best_model_entropy_loc_Cell_Phones_and_Accessories.pt' -lm='best_model_entropy_loc_Clothing_Shoes_and_Jewelry.pt' -lm='best_model_entropy_loc_Toys_and_Games.pt' -gm='best_model_entropy_globP_set_reviews_Food.pt'
+
+
+#distill knowledge
+  #OT-based
+CUDA_VISIBLE_DEVICES=2 python run_sinkhorn_confident_distill.py -loss_fn='ot' -pretrained='../saved_models/best_model_entropy_globP_set_reviews_Food.pt' -noisy_tranfer_labels='../noisy_labels/globF_Grocery_and_Gourmet_Food.pkl' -lm='../train_test_split/loc_Cell_Phones_and_Accessories.pkl' -lm='../train_test_split/loc_Clothing_Shoes_and_Jewelry.pkl' -lm='../train_test_split/loc_Toys_and_Games.pkl'  -lm='../train_test_split/globP_set_reviews_Food.pkl' -batch_size=1024 -epochs=20 -lr=0.001 -save_model_path='../saved_models' -metric='f1_macro' -result_path='../results.txt'
+CUDA_VISIBLE_DEVICES=3 python run_sinkhorn_confident_distill.py -loss_fn='entropy' -pretrained='../saved_models/best_model_entropy_globP_set_reviews_Food.pt' -noisy_tranfer_labels='../noisy_labels/globF_Grocery_and_Gourmet_Food.pkl' -lm='../train_test_split/loc_Cell_Phones_and_Accessories.pkl' -lm='../train_test_split/loc_Clothing_Shoes_and_Jewelry.pkl' -lm='../train_test_split/loc_Toys_and_Games.pkl'  -lm='../train_test_split/globP_set_reviews_Food.pkl' -batch_size=1024 -epochs=20 -lr=0.001 -save_model_path='../saved_models' -metric='f1_macro' -result_path='../results.txt'
+
